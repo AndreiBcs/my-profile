0262c8ebf97465e38f07f04307312d036069ff2e
AndreiBcs <andreibichis1@gmail.com>
2025-12-31 15:13:43 +0200

    commit

--- main.js
+++ main.js
@@ -1,36 +1,19 @@
-// Source - https://stackoverflow.com/a
-// Posted by georg, modified by community. See post 'Timeline' for change history
-// Retrieved 2025-12-29, License - CC BY-SA 3.0
+let canvas, ctx;
+window.onload = function() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw();
 
-var c = document.getElementById("cnv").getContext("2d");
-var f = Math.sqrt(2);
-
-function hor(x, y, len) {
-  if(len < 1) return;
-
-  c.beginPath();
-  c.moveTo(x - len/2, y);
-  c.lineTo(x + len/2, y);
-  c.stroke();
-
-  setTimeout(function() {
-    ver(x - len/2, y, len/f);
-    ver(x + len/2, y, len/f);
-  }, 500);
 }
+const x=100;
 
-function ver(x, y, len) {
-  if(len < 1) return;
-
-  c.beginPath();
-  c.moveTo(x, y - len/2);
-  c.lineTo(x, y + len/2);
-  c.stroke();
-
-  setTimeout(function() {
-    hor(x, y - len/2, len/f);
-    hor(x, y + len/2, len/f);
-  }, 500);
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(x, 200);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 5;
+    ctx.stroke();
 }
-
-hor(300, 90, 150)
